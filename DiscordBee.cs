483d5d816e8a55ae26116a8b2e5cfe7b35b03106
Svarr <svarr@gmx.de>
2021-08-29 15:00:33 +0200

    Stop DiscordBee from sending a presence update if not connected to Discord

--- DiscordBee.cs
+++ DiscordBee.cs
@@ -77,8 +77,8 @@
     private void ReadyCallback(object sender, ReadyMessage args)
     {
       Debug.WriteLine($"Ready. Connected to Discord Client with User: {args.User.Username}", "DiscordRpc");
-      UpdateDiscordPresence(_mbApiInterface.Player_GetPlayState());
       IsConnected = true;
+      UpdateDiscordPresence(_mbApiInterface.Player_GetPlayState());
     }
 
     private void ErrorCallback(object sender, ErrorMessage e)
@@ -140,6 +140,11 @@
           // _discordClient is either null or disposed
           InitialiseDiscordRpcClient();
         }
+        else
+		{
+          // _discordClient is up and trying to connect
+          return;
+		}
       }
       // perform some action depending on the notification type
       switch (type)
@@ -176,6 +181,11 @@
     private void UpdateDiscordPresence(PlayState playerGetPlayState)
     {
       Debug.WriteLine("DiscordBee: Updating Presence with PlayState {0}...", playerGetPlayState);
+      if (!IsConnected)
+	  {
+        Debug.WriteLine("Client not connected, not sending Presence update.", "DiscordBee");
+        return;
+	  }
       var metaDataDict = GenerateMetaDataDictionary();
 
       // Discord allows only strings with a min length of 2 or the update fails
